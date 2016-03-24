0be0d037ce4c997f1837ae7a2c1a0f5dc5867329
lzxm160 <lzxm160@sina.com>
2016-03-24 17:03:27 +0800

    credit on time

--- src/credit_ontime.hpp
+++ src/credit_ontime.hpp
@@ -15,52 +15,58 @@ public:
 	}
 	void start()
 	{
-
-	typedef tuple<unique_ptr<string>, unique_ptr<double> ,unique_ptr<string>> credit_tuple;
-	
-	//typedef tuple<string,double> credit_tuple;
-	vector<credit_tuple> credits;
-	string query_sql = "SELECT customer_credit_flow_id,balance,customer_master_id FROM " + get_config->m_database + "." + get_config->m_table + " where expire_date='" + m_today_string + "' and balance>0 and dr=0 and transaction_type=0";
-	cout << query_sql << endl;
-	m_conn->runQuery(&credits, query_sql.c_str());
-
-	BOOST_LOG_SEV(slg, boost_log->get_log_level()) << query_sql;
-	boost_log->get_initsink()->flush();
-	/********************************/
-	cout.setf(ios::showpoint); cout.setf(ios::fixed); cout.precision(8);
-	/********************************/
-
-	for (const auto& item : credits)
-	{
-		cout << item << endl;
-
-		string update_sql = "update " + get_config->m_database + "." + get_config->m_table + " set balance=0 where customer_credit_flow_id='" + *(std::get<0>(item))+"'";
-		cout << update_sql << endl;
-		string update_sql2;
 		try
 		{
-			m_conn->runCommand(update_sql.c_str());
-			//¸üÐÂÁíÒ»¸ö±í
-			update_sql2 = "update " + get_config->m_database + "." + get_config->m_table2 + " set credit_balance=0 where customer_master_id='" + *(std::get<2>(item))+"'";
-			cout << update_sql2 << endl;
-			m_conn->runCommand(update_sql2.c_str());
+			typedef tuple<unique_ptr<string>, unique_ptr<double> ,unique_ptr<string>> credit_tuple;
 			
-			BOOST_LOG_SEV(slg, boost_log->get_log_level()) << update_sql;
-			BOOST_LOG_SEV(slg, boost_log->get_log_level()) << update_sql2;
+			//typedef tuple<string,double> credit_tuple;
+			vector<credit_tuple> credits;
+			string query_sql = "SELECT customer_credit_flow_id,balance,customer_master_id FROM " + get_config->m_database + "." + get_config->m_table + " where expire_date='" + m_today_string + "' and balance>0 and dr=0 and transaction_type=0";
+			cout << query_sql << endl;
+			m_conn->runQuery(&credits, query_sql.c_str());
+
+			BOOST_LOG_SEV(slg, boost_log->get_log_level()) << query_sql;
 			boost_log->get_initsink()->flush();
-		}
+			/********************************/
+			cout.setf(ios::showpoint); cout.setf(ios::fixed); cout.precision(8);
+			/********************************/
+
+			for (const auto& item : credits)
+			{
+				cout << item << endl;
+
+				string update_sql = "update " + get_config->m_database + "." + get_config->m_table + " set balance=0 where customer_credit_flow_id='" + *(std::get<0>(item))+"'";
+				cout << update_sql << endl;
+				string update_sql2;
+				try
+				{
+					m_conn->runCommand(update_sql.c_str());
+					//¸üÐÂÁíÒ»¸ö±í
+					update_sql2 = "update " + get_config->m_database + "." + get_config->m_table2 + " set credit_balance=0 where customer_master_id='" + *(std::get<2>(item))+"'";
+					cout << update_sql2 << endl;
+					m_conn->runCommand(update_sql2.c_str());
+					
+					BOOST_LOG_SEV(slg, boost_log->get_log_level()) << update_sql;
+					BOOST_LOG_SEV(slg, boost_log->get_log_level()) << update_sql2;
+					boost_log->get_initsink()->flush();
+				}
+				catch (const MySqlException& e)
+				{
+					BOOST_LOG_SEV(slg, severity_level::error) << "(1)" << update_sql << "(2)" << update_sql2 << "(exception:)" << e.what();
+					boost_log->get_initsink()->flush();
+				}
+				
+
+				
+			}
+
+			credits.clear();
+			}
 		catch (const MySqlException& e)
 		{
-			BOOST_LOG_SEV(slg, severity_level::error) << "(1)" << update_sql << "(2)" << update_sql2 << "(exception:)" << e.what();
+			BOOST_LOG_SEV(slg, severity_level::error) <<"(exception:)" << e.what();
 			boost_log->get_initsink()->flush();
 		}
-		
-
-		
-	}
-
-	credits.clear();
-	
 	}
 private:
 	boost::shared_ptr<MySql> m_conn;
