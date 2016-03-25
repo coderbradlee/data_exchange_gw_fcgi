99e213e0137bcd20595bb5c34d69d47892894847
lzxm160 <lzxm160@sina.com>
2016-03-25 15:38:04 +0800

    d

--- src/orderbot.hpp
+++ src/orderbot.hpp
@@ -184,9 +184,9 @@ protected:
 							cout<<it->second.get<int>("distribution_center_id")<<endl;
 							cout<<it->second.get<string>("distribution_center_name")<<endl;
 							cout<<it->second.get<double>("inventory_quantity")<<endl;
-							//cout<<"--------------------------"<<endl;
+							//scout<<"--------------------------"<<endl;
 						}
-						cout<<"=========================="<<endl;
+						cout<<"#####################################################"<<endl;
 					}
 					
 					//cout<<m_data<<endl;
