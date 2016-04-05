17c5db9a43aa6df83ea77d29e35930883350b3e9
lzxm160 <lzxm160@sina.com>
2016-04-05 11:44:28 +0800

    add fcgi

---
+++ src/cgi.hpp
@@ -0,0 +1,97 @@
+#ifndef CGI_HPP
+#define	CGI_HPP
+
+#include  "include.hpp"
+//#define DEBUG
+#include "fcgio.h"
+#include <unistd.h>
+
+class fcgi_equest{
+
+    public:
+        fcgi_equest( FCGX_Request *fastcgi_request )
+            :fastcgi_request(fastcgi_request)
+        {
+
+
+        }
+
+        void reload(){
+
+            this->parameters.clear();
+
+            this->parameters[ "GATEWAY_INTERFACE" ] = FCGX_GetParam( "GATEWAY_INTERFACE", this->fastcgi_request->envp );
+            this->parameters[ "SERVER_SOFTWARE" ] = FCGX_GetParam( "SERVER_SOFTWARE", this->fastcgi_request->envp );
+            this->parameters[ "QUERY_STRING" ] = FCGX_GetParam( "QUERY_STRING", this->fastcgi_request->envp );
+            this->parameters[ "REQUEST_METHOD" ] = FCGX_GetParam( "REQUEST_METHOD", this->fastcgi_request->envp );
+            this->parameters[ "CONTENT_TYPE" ] = FCGX_GetParam( "CONTENT_TYPE", this->fastcgi_request->envp );
+            this->parameters[ "CONTENT_LENGTH" ] = FCGX_GetParam( "CONTENT_LENGTH", this->fastcgi_request->envp );
+            this->parameters[ "SCRIPT_FILENAME" ] = FCGX_GetParam( "SCRIPT_FILENAME", this->fastcgi_request->envp );
+            this->parameters[ "SCRIPT_NAME" ] = FCGX_GetParam( "SCRIPT_NAME", this->fastcgi_request->envp );
+            this->parameters[ "REQUEST_URI" ] = FCGX_GetParam( "REQUEST_URI", this->fastcgi_request->envp );
+            this->parameters[ "DOCUMENT_URI" ] = FCGX_GetParam( "DOCUMENT_URI", this->fastcgi_request->envp );
+            this->parameters[ "DOCUMENT_ROOT" ] = FCGX_GetParam( "DOCUMENT_ROOT", this->fastcgi_request->envp );
+            this->parameters[ "SERVER_PROTOCOL" ] = FCGX_GetParam( "SERVER_PROTOCOL", this->fastcgi_request->envp );
+            this->parameters[ "REMOTE_ADDR" ] = FCGX_GetParam( "REMOTE_ADDR", this->fastcgi_request->envp );
+            this->parameters[ "REMOTE_PORT" ] = FCGX_GetParam( "REMOTE_PORT", this->fastcgi_request->envp );
+            this->parameters[ "SERVER_ADDR" ] = FCGX_GetParam( "SERVER_ADDR", this->fastcgi_request->envp );
+            this->parameters[ "SERVER_PORT" ] = FCGX_GetParam( "SERVER_PORT", this->fastcgi_request->envp );
+            this->parameters[ "SERVER_NAME" ] = FCGX_GetParam( "SERVER_NAME", this->fastcgi_request->envp );
+
+            this->loadInput();
+
+        }
+
+
+        void loadInput(){
+
+            size_t number_of_bytes_requested = INPUT_BUFFER_SIZE;
+            size_t number_of_bytes_read;
+            size_t total_bytes_read = 0;
+
+            this->message_body = "";
+
+            do{
+
+                number_of_bytes_read = FCGX_GetStr( this->input_buffer, number_of_bytes_requested, fastcgi_request->in );
+                if( number_of_bytes_read <= 0 ){
+                    break;
+                }
+                total_bytes_read += number_of_bytes_read;
+
+                if( total_bytes_read > MAX_INPUT_MESSAGE_BODY ){
+                    this->message_body.append( "Max Body Length Reached" );
+                    break;
+                }
+
+                this->message_body.append( this->input_buffer, number_of_bytes_read );
+
+            }while( number_of_bytes_read == INPUT_BUFFER_SIZE );
+
+
+        }
+
+
+        string getParameterByName( string parameter_name ){
+
+            string parameter_value;
+            parameter_value = FCGX_GetParam( parameter_name.c_str(), fastcgi_request->envp );
+            return parameter_value;
+
+        }
+
+
+        map< string, string > parameters;
+        string message_body;
+
+    protected:
+        FCGX_Request *fastcgi_request;
+
+        char input_buffer[ INPUT_BUFFER_SIZE ];
+
+
+
+
+};
+#endif
+
--- src/main.cpp
+++ src/main.cpp
@@ -2,13 +2,76 @@
 #include "orderbot.hpp"
 #include "credit_ontime.hpp"
 #include "activemq.hpp"
-
+#include "cgi.hpp"
 
 int main()
 {
 	try
 	{
-		
+		{
+		 // Backup the stdio streambufs
+		    streambuf * cin_streambuf  = cin.rdbuf();
+		    streambuf * cout_streambuf = cout.rdbuf();
+		    streambuf * cerr_streambuf = cerr.rdbuf();
+
+		    FCGX_Request fastcgi_request;
+
+		    FCGX_Init();
+		    FCGX_InitRequest(&fastcgi_request, 0, 0);
+
+		    int x = 0;
+
+		    boost::shared_ptr<fcgi_equest> request = boost::shared_ptr<fcgi_equest>(new fcgi_equest( &fastcgi_request ));
+		    map<string,string>::iterator parameter_iterator;
+
+		    while( FCGX_Accept_r(&fastcgi_request) == 0 ){
+
+		        x++;
+
+		        request->reload();
+
+		        cout << request->message_body << endl;
+		       
+		        fcgi_streambuf cin_fcgi_streambuf( fastcgi_request.in );
+		        fcgi_streambuf cout_fcgi_streambuf( fastcgi_request.out );
+		        fcgi_streambuf cerr_fcgi_streambuf( fastcgi_request.err );
+
+		        cin.rdbuf( &cin_fcgi_streambuf );
+		        cout.rdbuf( &cout_fcgi_streambuf );
+		        cerr.rdbuf( &cerr_fcgi_streambuf );
+
+		        cout << "Content-type: text/html\r\n"
+		             << "\r\n"
+		             << "<html>\n"
+		             << "  <head>\n"
+		             << "    <title>Hello, World!</title>\n"
+		             << "  </head>\n"
+		             << "  <body>\n"
+		             << "    <h1>Hello, World!</h1>\n"
+		             ;
+
+		        cout << request->message_body << endl;
+
+
+		        cout << sizeof( FCGX_Request ) << endl;
+
+		        cout << x << "<br/>" << endl;
+
+		        for( parameter_iterator = request->parameters.begin(); parameter_iterator != request->parameters.end(); parameter_iterator++ ){
+		            cout << parameter_iterator->first << " => " << parameter_iterator->second << "<br/>" << endl;
+		        }
+
+		        cout << "</body></html>" << endl;
+
+		        // Note: the fcgi_streambuf destructor will auto flush
+		    }
+
+		    // restore stdio streambufs
+		    cin.rdbuf( cin_streambuf );
+		    cout.rdbuf( cout_streambuf );
+		    cerr.rdbuf( cerr_streambuf );
+
+		}
 		
 		{
 			boost::timer::cpu_timer pass;
@@ -35,8 +98,8 @@ int main()
 
 			//orderbot 接口
 			boost::shared_ptr<activemq> am = boost::shared_ptr<activemq>(new activemq(get_config->m_activemq_username, get_config->m_activemq_password, get_config->m_activemq_url));
-			am->request("POST", "/api/message/TEST", "type=queue", "body={message:\"test\"}");
-			cout<<am->get_data().length()<<":"<<am->get_data()<<endl;
+			//am->request("POST", "/api/message/TEST", "type=queue", "body={message:\"test\"}");
+			//cout<<am->get_data().length()<<":"<<am->get_data()<<endl;
 			//am->request("GET", "/api/message/TEST", "type=queue&clientId=consumerA", "");
 			//cout<<am->get_data().length()<<":"<<am->get_data()<<endl;
 			this_thread::sleep_for(chrono::seconds(10));
